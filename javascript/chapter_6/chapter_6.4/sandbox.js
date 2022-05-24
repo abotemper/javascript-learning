f83a0e0a26164ff95e6a2c197d4bb5688615c8f4
abotemper <93826732+abotemper@users.noreply.github.com>
2022-05-24 01:26:03 +0100

    Add files via upload

--- javascript/chapter_6/chapter_6.4/sandbox.js
+++ javascript/chapter_6/chapter_6.4/sandbox.js
@@ -1,8 +1,8 @@
 const button = document.querySelector('button');
 
-button.addEventListener('click', ()=>{
-    console.log('you click me');
-});
+// button.addEventListener('click', ()=>{
+//     console.log('you click me');
+// });
 
 const items = document.querySelectorAll('li');
 
@@ -15,3 +15,36 @@ items.forEach(item => {
         e.target.style.textDecoration = 'line-through';
     });
 });
+
+// const ul = document.querySelector('ul');
+// ul.remove();
+
+// items.forEach(item => {
+//     item.addEventListener('click',e => {
+//     e.target.remove();
+//     console.log('event in LI');
+//     e.stopPropagation();// we wont show the parent
+//     });
+// });
+
+const button1 = document.querySelector('button');
+const ul = document.querySelector('ul');
+
+// button.addEventListener('click', e => {
+//     ul.innerHTML += '<li>something new</li>';
+// });
+
+button.addEventListener('click', e => {
+    const li = document.createElement('li');
+    li.textContent = 'something new new to do';
+    // ul.append(li);
+    ul.prepend(li);
+});
+ul.addEventListener('click', e => {
+    // console.log('event in UL');
+    // console.log(e.target);
+    if(e.target.tagName === 'LI'){
+        e.target.remove();
+    }
+
+});//use ul ,and e.target to get the li
